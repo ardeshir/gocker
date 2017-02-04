3d5c4a676b7df21a1b64380222ff7cfcc6d6b97d
ardeshir sepahsalar <ardeshir.org@gmail.com>
2017-02-04 19:10:56 +0000

    adding child pid

--- main.go
+++ main.go
@@ -14,21 +14,38 @@ func main() {
    switch os.Args[1] {
     case "run":
         run()
+    case "child":
+        child()
     default:
        panic("Oops!")
     }
 }
 
 func run() {
-   fmt.Printf("running %v\n", os.Args[2:])
+   // fmt.Printf("running %v as pid %d\n", os.Args[2:], os.Getpid() )
+   cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+   cmd.Stdin = os.Stdin
+   cmd.Stderr = os.Stderr
+   cmd.Stdout = os.Stdout
+
+   cmd.SysProcAttr = &syscall.SysProcAttr {
+     Cloneflags:  syscall.CLONE_NEWUTS |  syscall.CLONE_NEWPID,
+   }
+   must(cmd.Run())
+}
+
+
+func child() {
+   fmt.Printf("running %v as pid %d\n", os.Args[2:], os.Getpid() )
    cmd := exec.Command(os.Args[2], os.Args[3:]...)
    cmd.Stdin = os.Stdin
    cmd.Stderr = os.Stderr
    cmd.Stdout = os.Stdout
 
-   cmd.SysProcAttr = &syscall.SysProcAttr { 
-     Cloneflags:  syscall.CLONE_NEWUTS,
-   }
+   /* cmd.SysProcAttr = &syscall.SysProcAttr {
+     Cloneflags:  syscall.CLONE_NEWUTS |  syscall.CLONE_NEWPID,
+   } */
+
    must(cmd.Run())
 }
 
